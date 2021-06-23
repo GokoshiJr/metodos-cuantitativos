49c77c9170b508c0af2e17667065aad5f01cf232
Julio Gonzalez <julioagonzalez18@gmail.com>
2021-06-23 17:34:19 -0400

    add: area_circulo example using monte carlo simulation

---
+++ simulacion/area_circulo.py
@@ -0,0 +1,40 @@
+import random, math
+
+""" 
+Simulacion Monte Carlo - Integracion con simulacion
+
+Calcule el area de un circulo que esta dentro de un cuadrado de area 1
+
+En la que hacemos lanzamientos de dardos al area del circulo
+
+Ejemplo
+Area del cuadrado = 1
+Area teorica del circulo = 0.7853982 
+"""
+
+ensayos = 1000000
+area_cuadrado = 2
+distancia = 0
+aciertos = 0
+area_circulo = 0
+linea = "-"*35
+
+for i in range(1, ensayos):
+  # generacion de coord aleatorias donde impactan los dardos
+  x = random.random() 
+  y = random.random()
+  # Calculamos la distancia donde cayo el dardo con el origen del circulo
+  distancia = math.sqrt(math.pow(x - 0.5, 2) + math.pow(y - 0.5, 2))
+  # Si el dardo cayo dentro del area del circulo sumamos +1
+  if distancia < 0.5:
+    aciertos += 1
+
+area_circulo = (aciertos / ensayos) * area_cuadrado
+print(linea)
+print("  Simulacion Monte Carlo")
+print(linea)
+print(f"  Lanzando {ensayos} dardos \n  en un cuadro de area {area_cuadrado} cm^2")
+print(f"  Dardos acertados {aciertos}")
+print(linea)
+print(f"  El area estimada del circulo es: \n  {area_circulo} cm^2")
+print(linea)
