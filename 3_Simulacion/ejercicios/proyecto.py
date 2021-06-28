2bce7a01894448f45f4103c4d43893d684ca59c2
Julio Gonzalez <julioagonzalez18@gmail.com>
2021-06-28 09:36:17 -0400

    fix: stable condition print

--- 3_Simulacion/ejercicios/proyecto.py
+++ 3_Simulacion/ejercicios/proyecto.py
@@ -19,12 +19,23 @@ sistema está ocupado?
 c) ¿Cuál es el número promedio de clientes en la cola?
 d) ¿Cuál es la probabilidad que hayan 10 clientes en la cola?
 
+Datos:
+
+Orientacion del Modelo: Este modelo es orientado a los procesos
+Tipo de similacion: De modelo de tiempo discreto
+
+Conclusion
+
+A la hora de tener un sistema, nosotros usamos estrategias de simulacion, para realizar un muestreo
+o estudio probabilistico de como se va a comportar nuestro sistema ante dichas entradas aleatorias,
+esto con el fin de ahorrar recursos, ya que al simular estamos usando un modelo que en efecto es mucho 
+más rentable en costos de operacion que los del modelo o sistema en la vida real, al evaluar las entradas
+podemos observar si tenemos alguna falla en el sistema y corregirla, esto es una buena estrategia que
+nos ahorra recursos, solo debemos programar el modelo y usar recursos computacionales.
+
 '''
 
 import math, random
-
-# Modelo orientado a los procesos
-# De modelo de tiempo discreto
 
 class M_M_1:
 
@@ -45,7 +56,7 @@ class M_M_1:
   def get_wq(self):
     return self.__wq
 
-  def get_ws(self): # ws
+  def get_ws(self):
     return (1 / (self.__miu - self.__lamb)) * 60
 
   def longitud_sistema(self): # Ls
@@ -75,33 +86,36 @@ class M_M_1:
     else:
       return False
 
-
-if __name__ == '__main__':
+def main():
 
   linea = "-"*50
+  lamb_total = 0
+  miu_total  = 0
+  estable = 0
+
   print(linea)
   print("  Asignacion 1 - Julio Gonzalez 28.195.303")
   print(linea)
   print("  Datos Generales")
   print(linea)
+  horas  = int(input("  Horas laborales que desea simular: "))
   miu    = int(input("  Capacidad de atencion del sistema: ")) # 150
   minimo = int(input("  Minimo de personas que llegan en promedio: "))  # 100
   maximo = int(input("  Maximo de personas que llegan en promedio: ")) # 150
   wq     = int(input("  Tiempo promedio de espera en la cola (minutos): ")) # 2 minutos
-  print("  Se realizara la simulacion 8 veces, \n  para simular el resultado de 8 horas laborales de nuestro restaurante")
+  print(linea)  
 
-  lamb_total = 0
-  miu_total = 0
-
-  for i in range (1, 9):
+  for i in range (1, horas + 1):
 
     servidor =  M_M_1(miu, maximo, minimo, wq, False)
 
     if (servidor.sistema_estable()):
 
+      estable += 1
       lamb_total += servidor.get_lambda()
       miu_total += servidor.get_miu()
 
+      print("\n\n\n")
       print(linea)
       print(f"  Datos Hora {i}")
       print(linea)
@@ -125,52 +139,39 @@ if __name__ == '__main__':
       print(f"  Numero promedio de clientes en la cola {servidor.longitud_cola():.2f} personas")
       # d) probabilidad de tener 10 clientes en la cola
       print(f"  Probabilidad de tener 10 clientes en la cola: {servidor.p_cliente_cola(10)*100:.2f} %")
+      print(linea)
     else:
-      print("  error el sistema esta saturado")
-      
-  print(linea)
-  print("  Resultados Finales y  Probabilidades - 8 horas laborales")
-  print(linea)
-  print(f"  miu promedio de 8 horas = {miu_total / 8}")
-  print(f"  lambda promedio de 8 horas = {lamb_total / 8}")
-  servidor_total = M_M_1(miu_total, 0, 0, wq, True, lamb_total)
-  print(f"  Ws = {servidor_total.get_ws():.2f} minutos")
-  print(f"  Wq = {servidor_total.get_wq():.2f} minutos")
-  print(f"  Ls = {servidor_total.longitud_sistema():.2f} clientes")
-  print(f"  Lq = {servidor_total.longitud_cola():.2f} clientes")
-  print(linea)
-  print(f"  Resultados")
-  print(linea)
-  # a) probabilidad de que el sistema este ocioso
-  print(f"  Sistema ocioso: {servidor_total.p_sistema_ocioso()*100:.2f} %")
-  print(f"  Sistema ocupado: {servidor_total.p_sistema_ocupado()*100:.2f} %")
-  # b) probabilidad de que un cliente llegue y tenga que esperar
-  print(f"  Probabilidad de que un cliente llegue y espere: {servidor_total.p_cliente_cola(1)*100:.2f} %")
-  print(f"  Probabilidad de tener 3 o menos clientes {servidor_total.p_cliente_cola_sumatoria(3)*100:.2f} %")
-  print(f"  Probabilidad de tener mas de 10 clientes {(1 - servidor_total.p_cliente_cola_sumatoria(10))*100:.2f} %")
-  # c) cual es el numero promedio de clientes en la cola
-  print(f"  Numero promedio de clientes en la cola {servidor_total.longitud_cola():.2f} personas")
-  # d) probabilidad de tener 10 clientes en la cola
-  print(f"  Probabilidad de tener 10 clientes en la cola: {servidor_total.p_cliente_cola(10)*100:.2f} %")
+      print("  Error el sistema esta saturado - rho > 1")
+      print(linea)
+      break
   
+  if (estable == horas):
+    print("\n\n\n")  
+    print(linea)
+    print(f"  Informe Final - Jornada de {horas} horas laborales")
+    print(linea)
+    print(f"  miu promedio de {horas} horas = {miu_total / horas:.2f}")
+    print(f"  lambda promedio de {horas} horas = {lamb_total / horas:.2f}")
+    servidor_total = M_M_1(miu_total, 0, 0, wq, True, lamb_total)
+    print(f"  Ws = {servidor_total.get_ws():.2f} minutos")
+    print(f"  Wq = {servidor_total.get_wq():.2f} minutos")
+    print(f"  Ls = {servidor_total.longitud_sistema():.2f} clientes")
+    print(f"  Lq = {servidor_total.longitud_cola():.2f} clientes")
+    print(linea)
+    print(f"  Resultados Finales")
+    print(linea)
+    # a) probabilidad de que el sistema este ocioso
+    print(f"  Sistema ocioso: {servidor_total.p_sistema_ocioso()*100:.2f} %")
+    print(f"  Sistema ocupado: {servidor_total.p_sistema_ocupado()*100:.2f} %")
+    # b) probabilidad de que un cliente llegue y tenga que esperar
+    print(f"  Probabilidad de que un cliente llegue y espere: {servidor_total.p_cliente_cola(1)*100:.2f} %")
+    print(f"  Probabilidad de tener 3 o menos clientes {servidor_total.p_cliente_cola_sumatoria(3)*100:.2f} %")
+    print(f"  Probabilidad de tener mas de 10 clientes {(1 - servidor_total.p_cliente_cola_sumatoria(10))*100:.2f} %")
+    # c) cual es el numero promedio de clientes en la cola
+    print(f"  Numero promedio de clientes en la cola {servidor_total.longitud_cola():.2f} personas")
+    # d) probabilidad de tener 10 clientes en la cola
+    print(f"  Probabilidad de tener 10 clientes en la cola: {servidor_total.p_cliente_cola(10)*100:.2f} %")
+    print(linea)
 
-''' Elaborar un programa Python modular parametrizado que permita simular procesos y evaluar los resultados obtenidos. Debe cumplir con los siguientes requisitos:
-
-Identificar la Orientación del Modelo:
-A los eventos
-A los procesos.
-Identificar el tipo de Simulación:
-De modelos de tiempo discreto
-Orientada a los intervalos
-Incluir las Rutinas Básicas de Simulación:
-Inicialización.
-Tiempo.
-Eventos.
-Informes.
-Simular y evaluar los resultados obtenidos para alguna de las situaciones planteadas en los problemas del Problemario de Teoría de Líneas de Espera (Unidad I).
-Código del modelo, enunciado del problema a simular, resultados y conclusiones obtenidas.
-Todo guardado en un archivo Word debidamente identificado.
-NOTA:
-Dos o más respuestas iguales o parecidas les será dividida la nota entre la cantidad de asignaciones en esa situación.
-Tamaño máximo permitido: 5 Mb.
-Debe tenerla lista para el día del Tercer Parcial cuando le será requerida '''
+if __name__ == "__main__":
+  main()    
